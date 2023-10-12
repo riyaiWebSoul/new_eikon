2da5bd87b30a5843a5938ba4bbcc88bbabc99b94
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-12 11:07:20 +0530

    api link change

--- server/app.js
+++ server/app.js
@@ -1,4 +1,67 @@
+const express = require('express');
+const app = express();
+const HomeRouter = require('./api/routes/home');
+const productRouter = require('./api/routes/product');
+const userRouter = require('./api/routes/user');
+const AboutRouter = require('./api/routes/about');
+const AppointmentRouter = require('./api/routes/appointment');
+const MedicalRouter = require('./api/routes/medical');
+const MapingEcommerceRouter = require('./api/routes/MapingEcommerce');
+const FooterRouter = require('./api/routes/footer');
+const EnquiryRouter = require('./api/routes/enquiry');
+const HealingTouch = require('./api/routes/healingTouch');
+const PatientReview = require('./api/routes/PatientReview');
+const DrList = require('./api/routes/drList');
+const LoginIdRouter = require('./api/routes/loginId');
+const ImageUploadRouter = require('./api/routes/imagesUpload');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { urlencoded, json } = require('body-parser');
+const cors = require('cors');
+// const fileUpload = require('express-fileupload');
 
-const handleDelete = async () => {
-    <h1>hello</h1>
-  };
+
+mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/eikon',{useNewUrlParser:true, useUnifiedTopology: true});
+
+
+mongoose.connection.on('error',err=>{
+  console.log('connection failed');
+});
+
+mongoose.connection.on('connected',()=>{
+  console.log('connected successfully with database');
+});
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+// app.use(fileUpload({
+//   useTempFiles:true
+// }))
+
+app.use(cors());
+
+app.use('/imageUploads', express.static('public/images'));
+  app.use('/products', productRouter.router);
+  app.use('/user', userRouter.router);
+  app.use('/about', AboutRouter.router);
+  app.use('/home', HomeRouter.router);
+  app.use('/appointments', AppointmentRouter.router);
+  app.use('/medical', MedicalRouter.router);
+  app.use('/MapingEcommerce', MapingEcommerceRouter.router);
+  app.use('/footer', FooterRouter.router);
+  app.use('/enquiry', EnquiryRouter.router);
+  app.use('/healingTouch', HealingTouch.router);
+  app.use('/PatientReview', PatientReview.router);
+  app.use('/drList', DrList.router);
+  app.use('/imageUpload', ImageUploadRouter.router);
+  app.use('/loginId', LoginIdRouter.router);
+  app.use('/images', express.static('public/images'));
+
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
+
+module.exports = app;
--- server/index.js
+++ server/index.js
@@ -7,34 +7,22 @@ const path = require('path');
 const server = express();
 const fs = require('fs');
 const http = require('http');
-const HomeRouter = require('./api/routes/home');
-const productRouter = require('./api/routes/product');
-const userRouter = require('./api/routes/user');
-const AboutRouter = require('./api/routes/about');
-const AppointmentRouter = require('./api/routes/appointment');
-const MedicalRouter = require('./api/routes/medical');
-const MapingEcommerceRouter = require('./api/routes/MapingEcommerce');
-const FooterRouter = require('./api/routes/footer');
-const EnquiryRouter = require('./api/routes/enquiry');
-const HealingTouch = require('./api/routes/healingTouch');
-const PatientReview = require('./api/routes/PatientReview');
-const DrList = require('./api/routes/drList');
-const LoginIdRouter = require('./api/routes/loginId');
-const ImageUploadRouter = require('./api/routes/imagesUpload');
+const app = require('./app');
+
 const PORT = process.env.PORT || 8080;
 
 // Connect to the MongoDB database
-async function connectToDatabase() {
-  try {
-    await mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/eikon', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('Database connected');
-  } catch (err) {
-    console.error('Error connecting to the database:', err);
-  }
-}
+// async function connectToDatabase() {
+//   try {
+//     await mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/eikon', {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('Database connected');
+//   } catch (err) {
+//     console.error('Error connecting to the database:', err);
+//   }
+// }
 
 // Middleware to parse JSON request bodies
 server.use(express.json());
@@ -44,11 +32,11 @@ server.use(morgan('default'));
 server.use(express.static('public'));
 server.use(cors());
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open', () => {
+//   console.log('Connected to MongoDB');
+// });
 
 // Configure Multer for handling file uploads
 // const storage = multer.diskStorage({
@@ -69,34 +57,16 @@ server.use(cors({
   credentials: true,
 }));
 // Define your routes using async functions
-async function setupRoutes() {
-  server.use('/imageUploads', express.static('public/images'));
-  server.use('/products', productRouter.router);
-  server.use('/user', userRouter.router);
-  server.use('/about', AboutRouter.router);
-  server.use('/home', HomeRouter.router);
-  server.use('/appointments', AppointmentRouter.router);
-  server.use('/medical', MedicalRouter.router);
-  server.use('/MapingEcommerce', MapingEcommerceRouter.router);
-  server.use('/footer', FooterRouter.router);
-  server.use('/enquiry', EnquiryRouter.router);
-  server.use('/healingTouch', HealingTouch.router);
-  server.use('/PatientReview', PatientReview.router);
-  server.use('/drList', DrList.router);
-  server.use('/imageUpload', ImageUploadRouter.router);
-  server.use('/loginId', LoginIdRouter.router);
-  server.use('/images', express.static('public/images'));
-}
 
 
 // Start the server on port 8080
 async function startServer() {
-  await connectToDatabase();
-  await setupRoutes();
+  // await connectToDatabase();
+  
 
   server.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
 }
 
-startServer();
+startServer(app);
