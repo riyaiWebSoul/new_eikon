2a666ac7ad13a56d6927233d9fd7d8ab05008cf6
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-12 12:24:27 +0530

    link

--- server/app.js
+++ server/app.js
@@ -21,7 +21,7 @@ const cors = require('cors');
 // const fileUpload = require('express-fileupload');
 
 
-mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/eikon',{useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/test',{useNewUrlParser:true, useUnifiedTopology: true});
 
 
 mongoose.connection.on('error',err=>{
