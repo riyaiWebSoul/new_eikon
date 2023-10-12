ad3bd86d2798cb8daa5f05f9723c63b1ef945bd5
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-12 11:39:47 +0530

    rout changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const multer = require('multer');
+ 
 const path = require('path');
 const server = express();
 const fs = require('fs');
