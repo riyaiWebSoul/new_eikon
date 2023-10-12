1fe3adbdbbea5c81b2cadf81f9d464ce9b198882
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-12 12:27:56 +0530

    app js

--- server/app.js
+++ server/app.js
@@ -18,50 +18,56 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { urlencoded, json } = require('body-parser');
 const cors = require('cors');
-// const fileUpload = require('express-fileupload');
 
+mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/test', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/test',{useNewUrlParser:true, useUnifiedTopology: true});
-
-
-mongoose.connection.on('error',err=>{
+mongoose.connection.on('error', err => {
   console.log('connection failed');
 });
 
-mongoose.connection.on('connected',()=>{
-  console.log('connected successfully with database');
+mongoose.connection.on('connected', () => {
+  console.log('connected successfully with the database');
 });
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// app.use(fileUpload({
-//   useTempFiles:true
-// }))
-
 app.use(cors());
 
 app.use('/imageUploads', express.static('public/images'));
-  app.use('/products', productRouter.router);
-  app.use('/user', userRouter.router);
-  app.use('/about', AboutRouter.router);
-  app.use('/home', HomeRouter.router);
-  app.use('/appointments', AppointmentRouter.router);
-  app.use('/medical', MedicalRouter.router);
-  app.use('/MapingEcommerce', MapingEcommerceRouter.router);
-  app.use('/footer', FooterRouter.router);
-  app.use('/enquiry', EnquiryRouter.router);
-  app.use('/healingTouch', HealingTouch.router);
-  app.use('/PatientReview', PatientReview.router);
-  app.use('/drList', DrList.router);
-  app.use('/imageUpload', ImageUploadRouter.router);
-  app.use('/loginId', LoginIdRouter.router);
-  app.use('/images', express.static('public/images'));
+app.use('/products', productRouter.router);
+app.use('/user', userRouter.router);
+app.use('/about', AboutRouter.router);
+app.use('/home', HomeRouter.router);
+app.use('/appointments', AppointmentRouter.router);
+app.use('/medical', MedicalRouter.router);
+app.use('/MapingEcommerce', MapingEcommerceRouter.router);
+app.use('/footer', FooterRouter.router);
+app.use('/enquiry', EnquiryRouter.router);
+app.use('/healingTouch', HealingTouch.router);
+app.use('/PatientReview', PatientReview.router);
+app.use('/drList', DrList.router);
+app.use('/imageUpload', ImageUploadRouter.router);
+app.use('/loginId', LoginIdRouter.router);
+app.use('/images', express.static('public/images'));
 
-app.get('*',(req,res,next)=>{
-  res.status(200).json({
-    message:'bad request'
-  })
-})
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error('Error occurred:', err);
+
+  // Check if the error status is 500 (Internal Server Error)
+  if (res.statusCode === 500) {
+    // Log the request details
+    console.error('Request URL:', req.originalUrl);
+    console.error('Request Method:', req.method);
+    console.error('Request Body:', req.body);
+  }
+
+  res.status(500).json({
+    message: 'Internal Server Error',
+  });
+});
 
 module.exports = app;
