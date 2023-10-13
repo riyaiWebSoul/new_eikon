d6c99569400c2148e3c4d1c6ec8c36b6a37677c4
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 13:40:40 +0530

    old name

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require('mongodb');
 
 module.exports = async (req, res) => {
-  let client;
   try {
     // Connection URL
     const url = 'mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/'; // Replace with your MongoDB connection string
@@ -10,37 +9,25 @@ module.exports = async (req, res) => {
     const dbName = 'eikon'; // Replace with your database name
 
     // Create a new MongoClient
-    client = new MongoClient(url, { useNewUrlParser: true });
+    const client = new MongoClient(url, { useNewUrlParser: true });
 
     // Connect to the server
     await client.connect();
-
+    
     // Select a specific database
     const db = client.db(dbName);
 
-    // Get a list of all collections in the database
-    const collections = await db.listCollections().toArray();
+    // Perform database operations here, e.g., retrieve data
+    const collection = db.collection(['footers','abouts','drlists']); // Replace with your collection name
+    const data = await collection.find({}).toArray(); // Retrieve all documents
 
-    // Define an object to store data from each collection
-    const allData = {};
-
-    // Iterate through the collections and retrieve data
-    for (const collectionInfo of collections) {
-      const collectionName = collectionInfo.name;
-      const collection = db.collection(collectionName);
-      const data = await collection.find({}).toArray();
-      allData[collectionName] = data;
-    }
+    // Close the connection
+    client.close();
 
     // Send the retrieved data as a response
-    res.status(200).json({ data: allData });
+    res.status(200).json({ data });
   } catch (error) {
     console.error("Function error:", error);
     res.status(500).json({ error: "Server error" });
-  } finally {
-    if (client) {
-      // Close the connection in the finally block to ensure it's always closed
-      client.close();
-    }
   }
 };
