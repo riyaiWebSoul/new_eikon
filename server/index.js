a6142c40d80111f52ae5d2142c24fa14b138998f
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 10:50:06 +0530

    change server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const app = require('./api/app'); 
- 
 module.exports = async (req, res) => {
-  const message = "Hello from a Vercel Serverless Function!";
-  res.status(200).json({ message });
+  try {
+    const message = "Hello from a Vercel Serverless Function!";
+    res.status(200).json({ message });
+  } catch (error) {
+    console.error("Function error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
 };
-
-
