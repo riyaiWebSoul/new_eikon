3c12b0b76e3be167ab7366f4bb6bac3b4b993aa9
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 13:43:41 +0530

    routes

--- server/index.js
+++ server/index.js
@@ -1,33 +1,49 @@
+const express = require('express');
 const { MongoClient } = require('mongodb');
+const app = express();
+const port = 3000; // Choose a port for your server
 
-module.exports = async (req, res) => {
+// Connection URL and Database Name
+const url = 'mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/';
+const dbName = 'eikon';
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Route to fetch data from MongoDB
+app.get('/data', async (req, res) => {
   try {
-    // Connection URL
-    const url = 'mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/'; // Replace with your MongoDB connection string
-
-    // Database Name
-    const dbName = 'eikon'; // Replace with your database name
-
     // Create a new MongoClient
     const client = new MongoClient(url, { useNewUrlParser: true });
 
     // Connect to the server
     await client.connect();
-    
+
     // Select a specific database
     const db = client.db(dbName);
 
-    // Perform database operations here, e.g., retrieve data
-    const collection = db.collection('footers','abouts','drlists'); // Replace with your collection name
-    const data = await collection.find({}).toArray(); // Retrieve all documents
+    // Access individual collections
+    const footersCollection = db.collection('footers');
+    const aboutsCollection = db.collection('abouts');
+    const drlistsCollection = db.collection('drlists');
+
+    // Perform database operations for each collection
+    const footersData = await footersCollection.find({}).toArray();
+    const aboutsData = await aboutsCollection.find({}).toArray();
+    const drlistsData = await drlistsCollection.find({}).toArray();
 
     // Close the connection
     client.close();
 
     // Send the retrieved data as a response
-    res.status(200).json({ data });
+    res.status(200).json({ footersData, aboutsData, drlistsData });
   } catch (error) {
     console.error("Function error:", error);
     res.status(500).json({ error: "Server error" });
   }
-};
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
