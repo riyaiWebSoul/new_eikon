14565832d1f63465c1c721a106d941183de468c6
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 10:43:25 +0530

    changes message

--- server/index.js
+++ server/index.js
@@ -1,21 +1,8 @@
-const app = require('./api/app'); // Import the Express app
-
+const app = require('./api/app'); 
+ 
 module.exports = async (req, res) => {
-  // This is your serverless function handler
-  // It uses the Express app for handling HTTP requests
-
-  // Implement any necessary CORS headers here if needed
-  res.setHeader('Access-Control-Allow-Origin', 'https://nwe-eikon.vercel.app'); // Replace with your desired origin
-
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(204).end();
-    return;
-  }
-
-  // Pass the request and response objects to your Express app
-  app(req, res);
+  const message = "Hello from a Vercel Serverless Function!";
+  res.status(200).json({ message });
 };
-  
+
+
