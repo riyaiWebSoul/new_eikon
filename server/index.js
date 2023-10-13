16a1490d335b1c6b00dc9a62872af4c0dde4fec2
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 12:10:21 +0530

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,17 @@
 module.exports = async (req, res) => {
   try {
-    const message = "Hello shravan aaj server chal jayega ";
-    res.status(200).json({ message });
+    const db = client.db('mongodb+srv://iwebsoul:ZkK7vXCmICDXqsM6@cluster0.meodf1o.mongodb.net/');
+    const collection = db.collection('eikon');
+
+    // Example: Insert a document
+    await collection.insertOne({ name: 'John' });
+
+    // Example: Query documents
+    const result = await collection.find({ name: 'John' }).toArray();
+
+    res.status(200).json({ data: result });
   } catch (error) {
-    console.error("Function error:", error);
-    res.status(500).json({ error: "Server error" });
+    console.error('Function error:', error);
+    res.status(500).json({ error: 'Server error' });
   }
 };
