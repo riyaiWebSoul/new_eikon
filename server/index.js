c33486af62e98e27f3ee55a7afa733cd52dff857
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 13:20:51 +0530

    connect express

--- server/index.js
+++ server/index.js
@@ -1,9 +1,4 @@
-module.exports = async (req, res) => {
-  try {
-    const message = "Hello  ";
-    res.status(200).json({ message });
-  } catch (error) {
-    console.error("Function error:", error);
-    res.status(500).json({ error: "Server error" });
-  }
-};
+const { createServer } = require('@vercel/node');
+const app = require('./api/app');
+
+module.exports = createServer(app);
