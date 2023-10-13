6027bca8d3ba596ed178ca01ad4d0d8d2cc27a79
riyaiWebSoul <riyasurena137@gmail.com>
2023-10-13 11:17:06 +0530

    remove setheader

--- server/index.js
+++ server/index.js
@@ -1,18 +1,7 @@
 const app = require('./api/app'); // Import the Express app
 
 module.exports = async (req, res) => {
-  // This is your serverless function handler
-
-  // Implement any necessary CORS headers here if needed
-  res.setHeader('Access-Control-Allow-Origin', 'https://nwe-eikon.vercel.app'); // Replace with your desired origin
-
-  if (req.method === 'OPTIONS') {
-    // Handle preflight requests
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.status(204).end();
-    return;
-  }
+ 
 
   try {
     const message = "Hello shravan aaj server chal jayega";
